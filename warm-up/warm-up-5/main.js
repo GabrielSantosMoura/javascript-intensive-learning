a9d8b670cc260ddbd7b108405e5809b0c95cc2e6
Gabriel Moura <gabrielmouraintj@gmail.com>
2026-04-26 19:53:48 -0300

    adding warm-up-5

---
+++ warm-up/warm-up-5/main.js
@@ -0,0 +1,14 @@
+
+for(let i = 1; i<31; i++){
+    if(i%2 === 0 && i%3 === 0){
+        console.log("eventhree")
+    }else if( i%2 === 0){
+        console.log("even")
+    }else if( i%3=== 0){
+        console.log("odd")
+    }else{
+        console.log(i)
+    }
+
+
+}
